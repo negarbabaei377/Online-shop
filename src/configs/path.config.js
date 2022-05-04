81856c51292955070f4f36aad5aff410a597319c
Negar Babaei <negarbabaei377@gmail.com>
2022-05-04 22:02:04 +0430

    feat : path config js file created

---
+++ src/configs/path.config.js
@@ -0,0 +1,13 @@
+export const PATH = {
+    HOME : "/" ,
+    LOGIN : "/login",
+    NOTFOUND : "*" ,
+    CART : "/cart",
+    CATEGORIES : "/categories",
+    DASHBOARD :"/dashboard",
+    PRODUCT : "/product",
+    CHECKOUT : "/checkout" ,
+    ORDERS : "orders" ,
+    PRODUCTMANAGMENT : "product-managment" ,
+    PRICEQUANTITY : "price-quantity"
+}
