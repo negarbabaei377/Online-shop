a22d1fd0d39a7c2a4c8ee48d7cf4b54cce599076
Negar Babaei <negarbabaei377@gmail.com>
2022-05-04 22:55:17 +0430

    feat : index created for components

---
+++ src/component/index.js
@@ -0,0 +1,4 @@
+export {ButtonComponent} from './Button/Button.component' ;
+export {ProductCardComponent} from './ProductCard/ProductCard.component';
+export {SliderComponent} from './Slider/Slider.component'
+
