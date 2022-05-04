c20190e23638decedefe2cc33e8c796a1180d3e1
Negar Babaei <negarbabaei377@gmail.com>
2022-05-04 22:01:51 +0430

    feat : route config js file created

---
+++ src/configs/route.config.js
@@ -0,0 +1,11 @@
+
+export const DEFAULT_PROPS = {
+    hasLayout : true,
+    Layout : 'User'
+}
+
+export const PROPS_TYPES ={
+    hasLayout : PropTypes.bool ,
+    Layout : PropTypes.string ,
+    Component : PropTypes.func.isRequired
+}
