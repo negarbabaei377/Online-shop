b8d2cc92238bf9ad0514356e0eaa370f4214c69d
Negar Babaei <negarbabaei377@gmail.com>
2022-05-04 22:31:47 +0430

    feat : private route component js file created

---
+++ src/routes/components/PrivateRoute/PrivateRoute.component.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {AdminLayout, UserLayout} from "layout";
+import {DEFAULT_PROPS, PROPS_TYPES} from "configs/route.config";
+import {IS_LOGIN} from "configs/variables.config";
+import {Navigate} from "react-router"
+import {PATH} from "configs/path.config";
+
+const TargetPage = ({Component, Layout, hasLayout}) => {
+    const isLogin = localStorage.getItem(IS_LOGIN)
+    if(isLogin){
+        return (
+            hasLayout ? (
+                Layout === "User" ? (
+                    <UserLayout>
+                        <Component/>
+                    </UserLayout>
+                ) : (
+                    <AdminLayout>
+                        <Component/>
+                    </AdminLayout>
+                )
+            ) : (
+                <Component/>
+            )
+        );
+    }else{
+       <Navigate to={PATH.LOGIN} />
+    }
+};
+
+
+export const PrivateRoute = (props) => {
+    const {Component, Layout, hasLayout} = props
+    return (
+        <TargetPage Component={Component}
+                    hasLayout={hasLayout}
+                    Layout={Layout}/>
+    );
+};
+
+PrivateRoute.defaultProps = DEFAULT_PROPS;
+PrivateRoute.propTypes = PROPS_TYPES ;
+
