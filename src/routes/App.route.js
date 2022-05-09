1bad789d62b01bd95b042ca6c72dcf2b010e40b6
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:52:13 +0430

    feat : layout removed from order & price quantity & product managment pages

--- src/routes/App.route.js
+++ src/routes/App.route.js
@@ -13,9 +13,9 @@ export const AppRoute = () => {
                 <Route path={PATH.CATEGORIES} element={<PublicRoute Component={Pages.CategoryPage}/>} />
                 <Route path={PATH.CHECKOUT} element={<PublicRoute Component={Pages.CheckoutPage}/>} />
                 <Route path={PATH.DASHBOARD} element={<PrivateRoute Component={Pages.DashboardPage} Layout={"Admin"}/>} >
-                    <Route pat={PATH.ORDERS} element={<PrivateRoute Component={Pages.OrdersPage} Layout={"Admin"}/> }/>
-                    <Route pat={PATH.PRICEQUANTITY} element={<PrivateRoute Component={Pages.PriceQuantityPage} Layout={"Admin"}/> }/>
-                    <Route pat={PATH.PRODUCTMANAGMENT} element={<PrivateRoute Component={Pages.ProductManagmentPage} Layout={"Admin"}/> }/>
+                    <Route path={PATH.ORDERS} element={<PrivateRoute Component={Pages.OrdersPage} Layout={"Admin"} hasLayout={false}/>} />
+                    <Route path={PATH.PRICEQUANTITY} element={<PrivateRoute Component={Pages.PriceQuantityPage} Layout={"Admin"} hasLayout={false}/>} />
+                    <Route path={PATH.PRODUCTMANAGMENT} element={<PrivateRoute Component={Pages.ProductManagmentPage} Layout={"Admin"} hasLayout={false}/>} />
                 </Route>
                 <Route path={PATH.LOGIN} element={<ProtectedRoute Component={Pages.LoginPage}/>} />
                 <Route path={PATH.NOTFOUND} element={<PublicRoute Component={Pages.NotfoundPage}/>} />
