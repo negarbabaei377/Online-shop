0b6229f9767d4762f6a1855ce1a1a36c7897712d
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:36:05 +0430

    feat : admin layout created

--- src/layout/Admin/Admin.layout.js
+++ src/layout/Admin/Admin.layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {FooterComponent, HeaderComponent} from "./components";
 
 export const AdminLayout = (props) => {
     return (
         <>
+            <HeaderComponent/>
             {props.children}
+            <FooterComponent/>
         </>
     );
 };
