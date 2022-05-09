a4a2ef64944efb4ba891e4f2cda713fcc3b0d59c
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:34:39 +0430

    feat : price quantity page for admin dashboard created

--- src/pages/PriceQuantity/PriceQuantity.page.js
+++ src/pages/PriceQuantity/PriceQuantity.page.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import style from './_PriceQuantity.module.scss'
+import {Container} from "@mui/material";
+import {StickyHeadTable} from "./component/index";
+import {LightTheme} from "assets/styles/themes/light/light.theme";
 
 export const PriceQuantityPage = (props) => {
     return (
         <div>
-
+            <Container>
+                <div className={style.title}>
+                    <h1>مدیریت موجودی و قیمت ها</h1>
+                    <button
+                        className={style.buttonHover}
+                        style={{backgroundColor: LightTheme.palette.button}}>ذخیره
+                    </button>
+                </div>
+                <StickyHeadTable/>
+            </Container>
         </div>
     );
 };
+
 
