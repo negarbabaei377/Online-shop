3a6e8c4c7179322c69a61cfecb92fd072b8fba54
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:29:16 +0430

    feat : user layout's header created

--- src/layout/User/components/Header/Header.component.js
+++ src/layout/User/components/Header/Header.component.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import style from './_Header.module.scss' ;
+import {Link} from "react-router-dom";
+import {PATH} from "configs/path.config";
+import {Container} from "@material-ui/core";
+import logoImage from 'assets/image/png/logo.png'
+import {LightTheme} from "assets/styles/themes/light/light.theme";
 
 export const HeaderComponent = (props) => {
     return (
-        <div>
-
+        <div className={style.header}>
+            <Container>
+                <div className={style.wrapper}>
+                    <div className={style.rightHead}>
+                        <Link to={PATH.HOME}><img src={logoImage}/></Link>
+                        <Link to={PATH.HOME}><h1>فروشگاه پاپیون گالری</h1></Link>
+                    </div>
+                    <div className={style.leftHead}>
+                        <Link style={{backgroundColor:LightTheme.palette.button}} to={PATH.LOGIN}>ورود</Link>
+                        <Link style={{backgroundColor:LightTheme.palette.button}} to={PATH.CART}>سبد خرید</Link>
+                    </div>
+                </div>
+            </Container>
         </div>
+
     );
 };
 
