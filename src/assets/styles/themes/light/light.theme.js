ddc1ed719bf79240cf34e9511791e83d987c17bb
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:38:44 +0430

    feat : light theme created

---
+++ src/assets/styles/themes/light/light.theme.js
@@ -0,0 +1,18 @@
+import {createTheme} from "@mui/material";
+
+export const LightTheme = createTheme({
+    palette: {
+        lightViolet: "#ddd6fe",
+        medViolet: "#c4b5fd",
+        lightGray: "#f4f4f5",
+        mediumGray: "#e4e4e7",
+        darkGray: "#d4d4d8",
+        dark: "#18181b",
+        whiteBack: "#fafafa",
+        redError: "#dc2626",
+        greenSuccess: "#65a30d",
+        button : "#9789ffb0" ,
+        buttonHover : "#d7d2f9a1"
+    } ,
+    direction : "rtl"
+})
