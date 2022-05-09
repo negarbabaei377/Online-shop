1de38d53215202c5b06db8274d85cea2075dcc21
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:35:09 +0430

    feat : product managment page for admin dashboard created

--- src/pages/ProductManagment/ProductManagment.page.js
+++ src/pages/ProductManagment/ProductManagment.page.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import style from './_ProductManagment.module.scss'
+import {Container} from "@mui/material";
+import {StickyHeadTable} from "./component/index";
+import {LightTheme} from "assets/styles/themes/light/light.theme";
 
 export const ProductManagmentPage = (props) => {
     return (
         <div>
-
+            <Container>
+                <div className={style.title}>
+                    <h1>مدیریت کالاها</h1>
+                    <button
+                        className={style.buttonHover}
+                        style={{backgroundColor: LightTheme.palette.button}}>افزودن کالا
+                    </button>
+                </div>
+                <StickyHeadTable/>
+            </Container>
         </div>
     );
 };
