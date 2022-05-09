4ea309d991af5d2f897484bd1e9507c191f59f9b
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:53:28 +0430

    feat : login api created

---
+++ src/api/admin.api.js
@@ -0,0 +1,11 @@
+import http from "service/http.service";
+import {LOGIN} from "../configs/endpoint.config";
+
+export const loginApi = async(data) =>{
+    try {
+        const response = await http.post(LOGIN , data)
+        return response;
+    }catch (error){
+        return Promise.reject(error)
+    }
+}
