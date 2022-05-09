ee112af7fc1155a61fff402b7c0bfc434e7c2973
Negar Babaei <negarbabaei377@gmail.com>
2022-05-10 00:08:10 +0430

    feat : cache provider | theme provider | toastify container added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {AppRoute} from "./routes/App.route";
 import {Provider} from "react-redux";
+import {store} from './redux/store';
+import "assets/styles/global.style.scss"
+import {ThemeProvider} from "@mui/material";
+import {LightTheme} from 'assets/styles/themes/light/light.theme'
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl"
+import {prefixer} from "stylis";
+import {CacheProvider} from "@emotion/react";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const cacheRtl = createCache({
+    key : "muirtl" ,
+    stylisPlugins : [prefixer , rtlPlugin] ,
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
     <Provider store={store}>
-        <AppRoute/>
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={LightTheme}>
+                <AppRoute/>
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={true}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </ThemeProvider>
+        </CacheProvider>
     </Provider>
 );
 
