e2bf206ec0264a19682cdf71373a6e1dd219a5b1
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:55:15 +0430

    feat : username input & password inout component created

---
+++ src/pages/Login/component/Password/Password.component.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {TextField} from "@material-ui/core";
+
+export const PasswordComponent = (props) => {
+
+    return (
+        <TextField
+            style={{width: '50%', marginTop: "1rem"}}
+            type="password"
+            id="outlined-basic"
+            name="password"
+            label="رمز عبور"
+            value={props.value}
+            onChange={props.onChange}
+            error={props.error}
+            helperText={props.helperText}
+            inputProps={{
+                style: {
+                    fontSize: 17,
+                    direction: 'ltr'
+                }
+            }}
+            InputLabelProps={{
+                style: {
+                    fontSize: 17,
+                    left: "auto",
+                    right: 0,
+                    direction: "rtl"
+                }
+            }}
+        />
+
+    )
+}
+
+
---
+++ src/pages/Login/component/Username/Username.component.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {TextField} from "@material-ui/core";
+
+export const UsernameComponent = (props) => {
+    return (
+        <TextField
+            style={{width: '50%', marginTop: "1rem"}}
+            type="username"
+            id="outlined-basic"
+            name="username"
+            label="نام کاربری"
+            value={props.value}
+            onChange={props.onChange}
+            error={props.error}
+            helperText={props.helperText}
+            inputProps={{
+                style: {
+                    fontSize: 17,
+                    direction: 'ltr'
+                }
+            }}
+            InputLabelProps={{
+                style: {
+                    fontSize: 17,
+                    left: "auto",
+                    right: 0,
+                    direction: "rtl"
+                }
+            }}
+        />
+
+    )
+}
+
+
