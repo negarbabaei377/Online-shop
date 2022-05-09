f7dcc08948b035e6476d8f175a0772a1170bee7a
Negar Babaei <negarbabaei377@gmail.com>
2022-05-10 00:04:02 +0430

    feat : user layout's header and footer components added

--- src/layout/User/User.layout.js
+++ src/layout/User/User.layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {HeaderComponent, FooterComponent} from "./components/index";
 
 export const UserLayout = (props) => {
     return (
         <>
+            <HeaderComponent/>
             {props.children}
+            <FooterComponent/>
         </>
     );
 };
