43d5259b261d076c5e0991333cebd785bcbe2d39
Negar Babaei <negarbabaei377@gmail.com>
2022-05-09 23:19:30 +0430

    feat : admin layout's header created

--- src/layout/Admin/components/Header/Header.component.js
+++ src/layout/Admin/components/Header/Header.component.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {PATH} from "configs/path.config";
+import {Button, ButtonGroup, Container} from "@material-ui/core";
+import style from './_Header.module.scss';
+import {useNavigate} from "react-router";
+import {LightTheme} from "assets/styles/themes/light/light.theme";
 
 export const HeaderComponent = (props) => {
+    const navigate = useNavigate()
     return (
-        <div>
+        <div className={style.admin}>
+            <Container>
+                <div className={style.wrapper}>
+                    <h1>پنل مدیریت فروشگاه پاپیون گالری</h1>
+                    <ButtonGroup size="large" variant='contained' aria-label="outlined button group">
+                        <Button style={{backgroundColor: LightTheme.palette.button , fontSize:"1.3rem"}} onClick={()=>navigate(PATH.PRODUCTMANAGMENT)}>کالاها</Button>
+                        <Button style={{backgroundColor: LightTheme.palette.button , fontSize:"1.3rem" , marginRight:"0.1rem"}} onClick={()=>navigate(PATH.PRICEQUANTITY)}>موجودی و قیمت ها</Button>
+                        <Button style={{backgroundColor: LightTheme.palette.button, fontSize:"1.3rem" , marginRight:"0.1rem"}}onClick={()=>navigate(PATH.ORDERS)}>سفارش ها</Button>
+                    </ButtonGroup>
+                    <div>
+                        <Link to={PATH.HOME}>بازگشت به سایت</Link>
+                        <button onClick={()=> {
+                            navigate()
 
+                        }}>خروج</button>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
