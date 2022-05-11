f255b5b1fc441655b8bdc5df507a464152c174b9
Negar Babaei <negarbabaei377@gmail.com>
2022-05-12 01:38:00 +0430

    feat : request to database for get fake data

--- src/routes/components/PrivateRoute/PrivateRoute.component.js
+++ src/routes/components/PrivateRoute/PrivateRoute.component.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {AdminLayout, UserLayout} from "layout";
 import {DEFAULT_PROPS, PROPS_TYPES} from "configs/route.config";
 import {IS_LOGIN} from "configs/variables.config";
-import {Navigate} from "react-router"
+import {Navigate} from "react-router";
 import {PATH} from "configs/path.config";
+import {UserApi} from "api/user.api";
 
 const TargetPage = ({Component, Layout, hasLayout}) => {
     const isLogin = JSON.parse(localStorage.getItem(IS_LOGIN))
     if (isLogin !== true) {
         return <Navigate to={PATH.LOGIN}/>
     }
+    UserApi().then(res=>res)
     return (
         hasLayout ? (
             Layout === "User" ? (
