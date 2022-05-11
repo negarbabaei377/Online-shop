73f53bccebc5866d9a2b4513e63d5958f6878f67
Negar Babaei <negarbabaei377@gmail.com>
2022-05-12 00:57:01 +0430

    feat : productState & categoryState created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import {orderReducer} from './reducers/orderReducer'
-import {combineReducers, createStore} from "redux";
+import {productReducer} from './reducers/productReducer'
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import {categoryReducer} from "./reducers/categoryReducer";
+import {orderReducer} from "./reducers/orderReducer";
 
 const reducers = combineReducers({
+    productState : productReducer ,
+    categoryState : categoryReducer,
     orderState : orderReducer
 })
 
-export const store = createStore(reducers)
+export const store = createStore(reducers , applyMiddleware(thunk))
