069204eb571d0973ec6d5d202ab02f18beba8e09
Negar Babaei <negarbabaei377@gmail.com>
2022-05-12 01:25:20 +0430

    feat : checkout file created

---
+++ src/pages/Orders/component/CheckOrder/CheckOrder.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "@material-ui/core";
+
+
+export const CheckOrderComponent = () => {
+    return (
+        <div>
+            <Link href="#" underline="none">بررسی سفارش</Link>
+        </div>
+    );
+};
+
