b15dbd7c5f30b05bd765e2aa90b02dd17d0bb92c
Negar Babaei <negarbabaei377@gmail.com>
2022-05-16 00:59:42 +0430

    feat : remove product component added

--- src/pages/ProductManagment/component/index.js
+++ src/pages/ProductManagment/component/index.js
@@ -1,3 +1,4 @@
-export {ProductManagementTable} from './ProductTable/ProductTable.component'
 export {RemoveProductComponent} from './RemoveProduct/RemoveProduct.component'
 export {EditProductComponent} from './EditProduct/EditProduct.component'
+export {ProductManagementTable} from './ProductTable/ProductTable.component'
+export {AddProductComponent} from './AddProduct/AddProduct.component'
