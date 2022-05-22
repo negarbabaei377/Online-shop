ba37652c9291674bec3318a4bd8f4b1d728c3102
Negar Babaei <negarbabaei377@gmail.com>
2022-05-22 15:58:41 +0430

    feat : link tag changed with mui icon (cart)

--- src/layout/User/components/Header/Header.component.js
+++ src/layout/User/components/Header/Header.component.js
@@ -5,8 +5,23 @@ import {PATH} from "configs/path.config";
 import {Container} from "@material-ui/core";
 import logoImage from 'assets/image/png/logo.png'
 import {LightTheme} from "assets/styles/themes/light/light.theme";
+import IconButton from "@mui/material/IconButton";
+import Badge from '@mui/material/Badge';
+import {styled} from '@mui/material/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import {useNavigate} from "react-router";
+
+const StyledBadge = styled(Badge)(({theme}) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
 
 export const HeaderComponent = (props) => {
+    const navigate = useNavigate()
     return (
         <div className={style.header}>
             <Container>
@@ -16,8 +31,21 @@ export const HeaderComponent = (props) => {
                         <Link to={PATH.HOME}><h1>فروشگاه پاپیون گالری</h1></Link>
                     </div>
                     <div className={style.leftHead}>
-                        <Link style={{backgroundColor:LightTheme.palette.button}} to={PATH.LOGIN}>ورود</Link>
-                        <Link style={{backgroundColor:LightTheme.palette.button}} to={PATH.CART}>سبد خرید</Link>
+                        <Link style={{backgroundColor: LightTheme.palette.button}}
+                              to={PATH.CATEGORIES}>دسته بندی</Link>
+                        <Link style={{backgroundColor: LightTheme.palette.button}}
+                              to={PATH.LOGIN}>ورود</Link>
+                        <IconButton aria-label="cart"
+                                    className={style.cartIcon}
+                                    onClick={()=>navigate(PATH.CART)}
+                        >
+                            <StyledBadge badgeContent={1}
+                                         color="secondary"
+                                         className={style.containerIcon}
+                            >
+                                <ShoppingCartIcon className={style.icon}/>
+                            </StyledBadge>
+                        </IconButton>
                     </div>
                 </div>
             </Container>
