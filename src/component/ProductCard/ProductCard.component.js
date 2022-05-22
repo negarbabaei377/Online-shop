38e5e0cc4bde9cc8f38e0c1f45f8a3b4bcbc9232
Negar Babaei <negarbabaei377@gmail.com>
2022-05-22 16:15:09 +0430

    feat : product card component created

--- src/component/ProductCard/ProductCard.component.js
+++ src/component/ProductCard/ProductCard.component.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import style from './_ProductCard.module.scss'
+import {REACT_APP_BASE_URL} from "configs/variables.config";
+import {PATH} from "configs/path.config";
+import {Link} from "react-router-dom";
+import {useNavigate} from "react-router";
 
 export const ProductCardComponent = (props) => {
+    const navigate = useNavigate()
     return (
         <div>
-
+            <div className={style.productCard} onClick={()=>navigate(PATH.PRODUCT)}>
+                <div className={style.productImage}><img src={REACT_APP_BASE_URL + "/files/" + props.image}/></div>
+                <span className={style.name}>{props.name}</span>
+                <span>{new Intl.NumberFormat().format(+props.price)} تومان</span>
+                <Link to={PATH.PRODUCT}>افزودن به سبد خرید</Link>
+            </div>
         </div>
     );
 };
