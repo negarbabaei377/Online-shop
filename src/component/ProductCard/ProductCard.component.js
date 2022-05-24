c13241db83af51d7a2fcfe9ef9d5b5efaaf9ffa8
Negar Babaei <negarbabaei377@gmail.com>
2022-05-24 15:49:55 +0430

    feat : error message set for product card

--- src/component/ProductCard/ProductCard.component.js
+++ src/component/ProductCard/ProductCard.component.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import style from './_ProductCard.module.scss'
 import {REACT_APP_BASE_URL} from "configs/variables.config";
-import {PATH} from "configs/path.config";
 import {Link} from "react-router-dom";
-import {useNavigate} from "react-router";
 
 export const ProductCardComponent = (props) => {
-    const navigate = useNavigate()
     return (
-        <div>
-            <div className={style.productCard} onClick={()=>navigate(PATH.PRODUCT)}>
-                <div className={style.productImage}><img src={REACT_APP_BASE_URL + "/files/" + props.image}/></div>
-                <span className={style.name}>{props.name}</span>
-                <span>{new Intl.NumberFormat().format(+props.price)} تومان</span>
-                <Link to={PATH.PRODUCT}>افزودن به سبد خرید</Link>
+        <Link to={"/product?id=" + props.id} className={style.cardLink}>
+            <div>
+                <div className={style.productCard}>
+                    <div className={style.productImage}><img src={REACT_APP_BASE_URL + "/files/" + props.image}/></div>
+                    <span className={style.name}>{props.name}</span>
+                    <span>{new Intl.NumberFormat().format(+props.price)} تومان</span>
+                    {props.count<6 && (props.count>0 ? <span className={style.count}>تنها {props.count} عدد در انبار باقی مانده است</span> : <span className={style.count}>اتمام موجودی محصول موردنظر</span>)}
+                    <button>افزودن به سبد خرید</button>
+                </div>
             </div>
-        </div>
+        </Link>
+
     );
 };
 
