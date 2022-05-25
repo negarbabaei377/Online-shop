fd5d2471b2d8a257dea1e425cd682909d59cb909
Negar Babaei <negarbabaei377@gmail.com>
2022-05-25 20:50:55 +0430

    feat : action and reducer for get single product created

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,8 +1,9 @@
-import {PRODUCT_GET_PRODUCT, PRODUCT_GET_SINGLE_PRODUCT} from "configs/variables.config";
-import {getSingleProduct, productApi} from "api/product.api"
+import {PRODUCT_GET_PRODUCT, PRODUCT_GET_SINGLE_CATEGORY, PRODUCT_GET_SINGLE_PRODUCT} from "configs/variables.config";
+import {getProductCategory, getSingleProduct, productApi} from "api/product.api"
 
 const productAction =(data)=>({type : PRODUCT_GET_PRODUCT, payload :data})
 const productActionId =(data)=>({type : PRODUCT_GET_SINGLE_PRODUCT, payload :data})
+const productActionCategory =(data)=>({type : PRODUCT_GET_SINGLE_CATEGORY, payload :data})
 
 export const getProduct =()=>{
     return(dispatch , getState)=>{
@@ -20,6 +21,18 @@ export const getSingleProductId =(id)=>{
         return getSingleProduct(id).then(res=>{
             const {data} = res
             dispatch(productActionId(data))
+        }).catch(error=>{
+            return Promise.reject(error)
+        })
+    }
+}
+
+export const getSingleCategory =(id , page)=>{
+    return(dispatch , getState)=>{
+        return getProductCategory(id , page).then(res=>{
+            const {data , headers} = res
+            dispatch(productActionCategory(data))
+            return headers
         }).catch(error=>{
             return Promise.reject(error)
         })
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,9 +1,10 @@
-import {PRODUCT_GET_PRODUCT, PRODUCT_GET_SINGLE_PRODUCT} from "configs/variables.config";
+import {PRODUCT_GET_PRODUCT, PRODUCT_GET_SINGLE_CATEGORY, PRODUCT_GET_SINGLE_PRODUCT} from "configs/variables.config";
 
 
 const initialState ={
     product : [],
-    singleProduct:[]
+    singleProduct:[],
+    singleCategory:[]
 }
 
 export const productReducer = (state = initialState , action)=>{
@@ -12,6 +13,8 @@ export const productReducer = (state = initialState , action)=>{
             return {...state , product : action.payload}
         case (PRODUCT_GET_SINGLE_PRODUCT):
             return {...state , singleProduct : action.payload}
+        case (PRODUCT_GET_SINGLE_CATEGORY):
+            return {...state , singleCategory : action.payload}
         default :
             return state
     }
